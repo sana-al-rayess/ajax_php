8f3f43b21f6015af5162823369a3810c079c4daa
Sana Rayess <rayess.sana@gmail.com>
2023-03-10 00:31:25 +0200

    edited api.php

--- api.php
+++ api.php
@@ -6,23 +6,3 @@ $db_name = "news_db";
 
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
 
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "SELECT * FROM news";
-$result = mysqli_query($conn, $sql);
-
-$news = array();
-
-if (mysqli_num_rows($result) > 0) {
-	while ($row = mysqli_fetch_assoc($result)) {
-		$news[] = $row;
-	}
-}
-
-mysqli_close($conn);
-
-header('Content-Type: application/json');
-echo json_encode($news);
-?>
