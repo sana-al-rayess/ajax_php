78a3f1b52cf45f11c2112d55e0d72a1c3009ae61
Sana Rayess <rayess.sana@gmail.com>
2023-03-10 00:29:24 +0200

    added scripts and a div container

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>News Website</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script src="index.js"></script>
+</head>
+<body>
+	<div id="news-container"></div>
+</body>
+</html>
